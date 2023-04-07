2ebde475e45046c6f772e15d89163a9d5adb31e6
Calvin Chern <calvin.chern24@gmail.com>
2023-04-06 22:46:44 -0400

    Update server.js to use apollo-server-express

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-const { ApolloServer } = require('apollo-server');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
-
+const path = require('path');
 const typeDefs = require('./server/schemas/typeDefs');
 const resolvers = require('./server/schemas/resolvers');
-// const db = require('./server/config/connection');
-
+const db = require('./server/config/connection');
+const app = express();
 const PORT = process.env.port || 3001;
+
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 const startApolloServer = async (typeDefs, resolvers) => {
-  await server.listen(PORT, () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
   })
-};
+  };
   
 // Call async function to start the server
 startApolloServer(typeDefs, resolvers);

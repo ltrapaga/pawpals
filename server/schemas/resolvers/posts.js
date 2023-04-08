b5e8d08203f0854d4ef0f835521b1a607baca58e
Laura Trapaga <trapaga9@gmail.com>
2023-04-08 17:02:29 -0400

    Create `deletePost` mutation     Add `deletePost` function to resolvers/posts to delete a single post by index     Edit User and Post models

--- server/schemas/resolvers/posts.js
+++ server/schemas/resolvers/posts.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server-express");
+
 const Post = require("../../models/Post");
 const authMiddleware = require("../../utils/auth");
 
@@ -5,6 +7,7 @@ module.exports = {
   Query: {
     async getPosts() {
       try {
+        // Get all posts sorted from newest to oldest
         const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (err) {
@@ -39,6 +42,21 @@ module.exports = {
       });
       const post = await newPost.save();
       return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = authMiddleware(context);
+      try {
+        const post = await Post.findById(postId);
+        // Only allow a post to be deleted by the user that created it
+        if (user.username === post.username) {
+          await Post.deleteOne({ _id: postId });
+          return "Post deleted";
+        } else {
+          throw new AuthenticationError("Action unavailable");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 };

25fb75625e42ee0cfd388213e144915271db1a78
Laura Trapaga <trapaga9@gmail.com>
2023-04-15 15:21:28 -0400

    Fix server.js

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.use(express.json());
 
 // Serve client/build as static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 const startApolloServer = async (typeDefs, resolvers) => {
